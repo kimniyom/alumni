d6c3696619251fea6f30212953d39c8cb9f3a8b1
kimniyom <kimniyomclub@gmail.com>
2015-05-10 15:04:39 +0700

    new commit

--- protected/controllers/BackofficeController.php
+++ protected/controllers/BackofficeController.php
@@ -7,7 +7,28 @@ class BackofficeController extends Controller {
     public function actionGet_agent() {
         $agent = new CompanyAgent();
         $data['agent'] = $agent->findAll();
-        $this->render('//user/agent', $data);
+        $this->render('//backoffice/agent', $data);
+    }
+
+    public function actionDetail_agent() {
+        $id = $_GET['id'];
+        $agent = new CompanyAgent();
+        $data['agent'] = $agent->Get_agent($id);
+        $this->render('//backoffice/detail_agent', $data);
+    }
+
+    public function actionActive_user() {
+        $id = $_POST['id'];
+        $data = array('active' => '1');
+        Yii::app()->db->createCommand()
+                ->update("company_agent", $data, "id = '$id' ");
+    }
+
+    public function actionUnactive_user() {
+        $id = $_POST['id'];
+        $data = array('active' => '0');
+        Yii::app()->db->createCommand()
+                ->update("company_agent", $data, "id = '$id' ");
     }
 
 }
