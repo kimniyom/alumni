d44b74af289fb0d90a002185281d3d4a2e7bd581
kimniyom <kimniyomclub@gmail.com>
2015-05-13 00:18:34 +0700

    new commit

--- protected/views/privilege/box_user.php
+++ protected/views/privilege/box_user.php
@@ -4,14 +4,20 @@
             <h3 class="box-title"><i class="fa fa-user"></i> ข้อมูลผู้ใช้งาน</h3>
         </div>
         <div class="box-body">
-            <i class="fa fa-group"></i> <?php echo Yii::app()->session['agent_name']; ?><br/>
-            <i class="fa fa-compass"></i> <?php echo Yii::app()->session['company']; ?><br/>
-            <i class="fa fa-user-secret"></i> ตัวแทนบริษัท
+            <i class="fa fa-group"></i> ชื่อใช้งาน : <?php echo Yii::app()->session['agent_name']; ?><br/>
+            <i class="fa fa-compass"></i> บริษัท : <?php echo Yii::app()->session['company']; ?><br/>
+            <i class="fa fa-user-secret"></i> สถานะ : ตัวแทนบริษัท
             <br/><br/>
-            <a href="<?php echo Yii::app()->createUrl('frontend/user/detail_agent')?>" style=" float: right;"> 
-                <i class="fa fa-user"></i> ข้อมูลส่วนตัว</a>
-            <br/>
+
         </div><!-- /.box-body -->
+        <div class="box-footer">
+
+            <a href="<?php echo Yii::app()->createUrl('frontend/user/detail_agent') ?>" > 
+                <i class="fa fa-user"></i> ข้อมูลส่วนตัว</a><br/>
+            <a href="<?php echo Yii::app()->createUrl('frontend/user/edit_password&id=' . Yii::app()->session['agent_id']); ?>" > 
+                <i class="fa fa-unlock"></i> แก้ไขรหัสผ่าน</a>
+
+        </div>
     </div><!-- /.box -->
 </div>
 
