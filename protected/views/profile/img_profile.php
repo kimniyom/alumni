f7d450b81fce4d67a31d28d40e6f052facda223a
kimniyom <kimniyomclub@gmail.com>
2015-05-13 00:18:28 +0700

    new commit

--- protected/views/profile/img_profile.php
+++ protected/views/profile/img_profile.php
@@ -1,3 +1,17 @@
 <center>
-    <img src="<?php echo Yii::app()->baseUrl; ?>/assets/jquery.picture.cut/uploads/<?php echo $img; ?>" class="img-responsive">
+    <!-- normal -->
+    <div class="ih-item square colored effect13 top_to_bottom" style=" border: none; width: 100%; height: 100%;">
+        <a href="Javascript:void(0);" onclick="edit_img_profile();">
+            <div class="img" style=" border: none; width: 100%;">
+                <img src="<?php echo Yii::app()->baseUrl; ?>/assets/jquery.picture.cut/uploads/<?php echo $img; ?>" class="img-responsive">
+            </div>
+            <div class="info">
+                <h3>
+                    <i class="fa fa-camera fa-3x"></i><br/><br/>
+                    แก้ไขรูปภาพประจำตัว
+                </h3>
+            </div></a></div>
+    <!-- end normal -->
 </center>
+
+
