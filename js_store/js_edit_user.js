0f4d9450851c62520768e5efb5e66376a3b82372
kimniyom <kimniyomclub@gmail.com>
2015-05-13 00:17:27 +0700

    new commit

---
+++ js_store/js_edit_user.js
@@ -0,0 +1,60 @@
+function save_edit_user() {
+    var url = 'index.php?r=frontend/user/save_edit_user';
+    //alert(url);
+    var id = $("#agent_id").val();
+    var shot_name = $('#shot_name').val();
+    var name = $('#name').val();
+    var lname = $('#lname').val();
+    //var username = $('#username').val();
+    //var password = $('#password').val();
+    var tel = $('#tel').val();
+    var mobile = $('#mobile').val();
+    var company = $('#company').val();
+    var address = $('#address').val();
+
+    if (name == '') {
+        $("#name").focus();
+        return false;
+    }
+
+    if (lname == '') {
+        $("#lname").focus();
+        return false;
+    }
+
+    if (tel == '') {
+        $("#tel").focus();
+        return false;
+    }
+
+    if (mobile == '') {
+        $("#mobile").focus();
+        return false;
+    }
+
+    if (company == '') {
+        $("#company").focus();
+        return false;
+    }
+
+    if (address == '') {
+        $("#address").focus();
+        return false;
+    }
+
+
+    var data = {
+        id: id,
+        shot_name: shot_name,
+        name: name,
+        lname: lname,
+        tel: tel,
+        mobile: mobile,
+        company: company,
+        address: address
+    }
+    $.post(url, data, function (success) {
+        window.location = "index.php?r=frontend/user/detail_agent";
+    });
+}
+
---
+++ js_store/js_profile_collegian.js
@@ -0,0 +1,88 @@
+//  Function Create By Kimniyom 12-05-2015
+
+$(document).ready(function () {
+    codeline_up1();
+});
+
+function edit_img_profile() {
+    $("#from_img_profile").modal();
+}
+
+function autoload_img_profile() {
+    var loading = "<center><div class='overlay'><i class='fa fa-refresh fa-spin'></i></div><center>";
+    $("#show_img_profile").html(loading);
+    var url = "index.php?r=frontend/collegian/img_profile";
+    var collegiancode = $("#collegian_code").val();
+    var data = {collegian_code: collegiancode};
+    $.post(url, data, function (success) {
+        $("#show_img_profile").html(success);
+    });
+}
+
+function autoload_imf_profile() {
+    var url = "index.php?r=frontend/collegian/img_profile";
+    var colegiancode = $("#collegian_code").val();
+    var data = {collegian_code: colegiancode};
+    $.post(url, data, function (success) {
+        $("#img_profile").html(success);
+    });
+}
+
+//ดึงข้อมูลพี่รหัสมาแสดง
+function get_senior() {
+    var loading = "<center><div class='overlay'><i class='fa fa-refresh fa-spin'></i></div><center>";
+    $("#senior_code").html(loading);
+    var url = "index.php?r=frontend/collegian/senior";
+    var collegiancode = $("#collegian_code").val();
+    var data = {collegian_code: collegiancode};
+    $.post(url, data, function (success) {
+        $("#senior_code").html(success);
+    });
+}
+
+//เปิด dialog เพื่อเพิ่ม พี่รหัส
+function dialog_senior() {
+    var loading = "<center><div class='overlay'><i class='fa fa-refresh fa-spin'></i></div><center>";
+    $("#show_senion").html(loading);
+    var url = "index.php?r=frontend/collegian/get_senior_all";
+    var collegiancode = $("#collegian_code").val();
+    var data = {collegian_code: collegiancode};
+    $.post(url, data, function (success) {
+        $("#show_senion").html(success);
+    });
+    $("#dialog_senior").modal();
+}
+
+function add_senior(line_id, senior_code, gennumber) {
+    var collegian_code = $("#collegian_code").val();
+    var senior_code = senior_code;
+    var GenNumber = gennumber;
+    var line_id = line_id;
+
+    var url = "index.php?r=frontend/collegian/save_senior";
+    var data = {
+        collegian_code: collegian_code,
+        senior_code: senior_code,
+        GenNumber: GenNumber,
+        line_id: line_id
+    };
+
+    $.post(url, data, function (success) {
+        get_senior();
+        $("#dialog_senior").modal("hide");
+    });
+}
+
+
+//ดึงข้อมูลพี่รหัสมาแสดง
+function codeline_up1() {
+    var loading = "<center><div class='overlay'><i class='fa fa-refresh fa-spin'></i></div><center>";
+    $("#codeline_up1").html(loading);
+    var url = "index.php?r=frontend/collegian/codeline_up1";
+    var collegiancode = $("#collegian_code").val();
+    var data = {collegian_code: collegiancode};
+    $.post(url, data, function (success) {
+        $("#codeline_up1").html(success);
+    });
+}
+
