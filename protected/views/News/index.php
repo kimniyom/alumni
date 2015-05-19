4864b0f276f8cfc922a06bcb84e3ae7c78fafdb7
kimniyom <kimniyomclub@gmail.com>
2015-05-19 16:57:43 +0700

    ดึงข่าวมาแสดง

--- protected/views/News/index.php
+++ protected/views/News/index.php
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-    $(document).ready(function(){
+    $(document).ready(function () {
         $("#News").dataTable();
     });
 </script>
@@ -7,30 +7,30 @@
     <div class="panel panel-heading">
         หน้าประกาศข่าว
     </div>
-<div class="panel-body">
-    <table class="display cell-border" id="News">
-    <thead>
-        <tr>
-            <th>News_ID</th>
-            <th>News_Head</th>
-            <th>News_Icon_id</th>
-            <th>News_Catagory_id</th>
-            <th>News_Group_id</th>
-            <th>News_Owner</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($News as $rs): ?>
-        <tr>
-            <td><?php echo $rs['News_id'];?></td>
-            <td><?php echo $rs['News_Head'];?></td>
-            <td><?php echo $rs['News_Icon_id'];?></td>
-            <td><?php echo $rs['News_Catagory_id'];?></td>
-            <td><?php echo $rs['News_Group_id'];?></td>
-            <td><?php echo $rs['News_Owner'];?></td>
-        </tr>
-        <?php endforeach;?>
-    </tbody>
-</table>
-</div>
+    <div class="panel-body">
+        <table class="display cell-border" id="News">
+            <thead>
+                <tr>
+                    <th>News_ID</th>
+                    <th>News_Head</th>
+                    <th>News_Icon_id</th>
+                    <th>News_Catagory_id</th>
+                    <th>News_Group_id</th>
+                    <th>News_Owner</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($News as $rs): ?>
+                    <tr>
+                        <td><?php echo $rs['News_id']; ?></td>
+                        <td><?php echo $rs['News_Head']; ?></td>
+                        <td><?php echo $rs['News_Icon_id']; ?></td>
+                        <td><?php echo $rs['News_Catagory_id']; ?></td>
+                        <td><?php echo $rs['News_Group_id']; ?></td>
+                        <td><?php echo $rs['News_Owner']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
