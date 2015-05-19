c3473b602505645a58d51d93001b24968eca6272
kimniyom <kimniyomclub@gmail.com>
2015-05-19 16:18:58 +0700

    new commit

---
+++ protected/controllers/frontend/AptitudeController.php
@@ -0,0 +1,63 @@
+<?php
+
+class AptitudeController extends Controller {
+
+    public $layout = "profile";
+
+    public function actionIndex() {
+        $aptitude = new Aptitude();
+        $collegian_code = $_GET['collegian_code'];
+        $data['collegian_code'] = $collegian_code;
+        $data['aptitude'] = $aptitude->findAll("collegian_code = '$collegian_code' ");
+
+        $this->render('//aptitude/index', $data);
+    }
+
+    public function actionGet_aptitude() {
+        $aptitude = new Aptitude();
+        $collegian_code = $_POST['collegian_code'];
+        $data['collegian_code'] = $collegian_code;
+        $data['aptitude'] = $aptitude->findAll("collegian_code = '$collegian_code' ");
+
+        $this->renderPartial('//aptitude/view', $data);
+    }
+
+    public function actionSave_aptitude() {
+        $Columns = array(
+            "collegian_code" => $_POST['collegian_code'],
+            "aptitude" => $_POST['aptitude'],
+            "d_update" => date("Y-m-d H:i:s")
+        );
+
+        Yii::app()->db->createCommand()
+                ->insert("aptitude", $Columns);
+    }
+
+    public function actionEdit() {
+        $aptitude = new Aptitude();
+        $collegian_code = $_GET['collegian_code'];
+        $id = $_GET['id'];
+        $data['collegian_code'] = $collegian_code;
+        $data['aptitude'] = $aptitude->find("id = '$id' ");
+        $data['aptitudeAll'] = $aptitude->findAll("collegian_code = '$collegian_code' ");
+        $this->render('//aptitude/edit', $data);
+    }
+
+    public function actionEdit_aptitude() {
+        $id = $_POST['id'];
+        $Columns = array(
+            "aptitude" => $_POST['aptitude'],
+            "d_update" => date("Y-m-d H:i:s")
+        );
+
+        Yii::app()->db->createCommand()
+                ->update("aptitude", $Columns, "id = '$id' ");
+    }
+
+    public function actionDelete_aptitude() {
+        $id = $_POST['id'];
+        Yii::app()->db->createCommand()
+                ->delete("aptitude", "id = '$id' ");
+    }
+
+}
---
+++ protected/controllers/frontend/Collegian_etcController.php
@@ -0,0 +1,66 @@
+<?php
+
+class Collegian_etcController extends Controller {
+
+    public $layout = "profile";
+
+    public function actionIndex() {
+        $Etc = new CollegianEtc();
+        $collegian_code = $_GET['collegian_code'];
+        $data['collegian_code'] = $collegian_code;
+        $data['etc'] = $Etc->findAll("collegian_code = '$collegian_code' ");
+
+        $this->render('//collegian_etc/index', $data);
+    }
+
+    public function actionGet_etc() {
+        $Etc = new CollegianEtc();
+        $collegian_code = $_POST['collegian_code'];
+        $data['collegian_code'] = $collegian_code;
+        $data['etc'] = $Etc->findAll("collegian_code = '$collegian_code' ");
+
+        $this->renderPartial("//collegian_etc/view", $data);
+    }
+
+    public function actionSave_collegian_etc() {
+        $columns = array(
+            "collegian_code" => $_POST['collegian_code'],
+            "etc" => $_POST['etc'],
+            "active" => "0",
+            "d_update" => date("Y-m-d H:i:s")
+        );
+
+        Yii::app()->db->createCommand()
+                ->insert("collegian_etc", $columns);
+    }
+
+    public function actionEdit() {
+        $Etc = new CollegianEtc();
+        $collegian_code = $_GET['collegian_code'];
+        $id = $_GET['id'];
+        $data['collegian_code'] = $collegian_code;
+        $data['etc'] = $Etc->find("id = '$id' ");
+        $data['etcAll'] = $Etc->findAll("collegian_code = '$collegian_code' ");
+
+        $this->render("//collegian_etc/edit", $data);
+    }
+
+    public function actionEdit_collegian_etc() {
+        $id = $_POST['id'];
+        $columns = array(
+            "etc" => $_POST['etc'],
+            "d_update" => date("Y-m-d H:i:s")
+        );
+
+        Yii::app()->db->createCommand()
+                ->update("collegian_etc", $columns, "id = '$id' ");
+    }
+
+    public function actionDelete_collegian_etc() {
+        $id = $_POST['id'];
+
+        Yii::app()->db->createCommand()
+                ->delete("collegian_etc", "id = '$id' ");
+    }
+
+}
