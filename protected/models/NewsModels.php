640bfdd01bea880911d52bbf87f4b6d98f0517fe
kimniyom <kimniyomclub@gmail.com>
2015-05-21 12:50:27 +0700

    new commit

--- protected/models/NewsModels.php
+++ protected/models/NewsModels.php
@@ -11,4 +11,11 @@ class NewsModels extends CActiveRecord {
         return 'News';
     }
     
+    public function Get_News_All(){
+        $query = "SELECT n.*,c.collegian_name,c.collegian_lname
+        FROM News n INNER JOIN collegian c ON n.News_Owner = c.collegian_code";
+        
+        return Yii::app()->db->createCommand($query)->queryAll();
+    }
+    
 }
