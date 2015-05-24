e34a24389d734d07a5c0ae44a05faad48f838ee1
kimniyom <kimniyomclub@gmail.com>
2015-05-24 22:45:54 +0700

    new commit

--- protected/views/privilege/head_menu_admin.php
+++ protected/views/privilege/head_menu_admin.php
@@ -1,8 +1,23 @@
-<?php
+<nav class="collapse navbar-collapse" role="navigation">
+    <ul class="nav navbar-nav">
+        <li>
+            <a href="index.php?r=site"><b><i class="fa fa-home"></i> หน้าเว็บ</b></a>
+        </li>
+        <li style="padding: 0px;">
+            <a href="#stories"><i class="glyphicon glyphicon-comment"></i> ข้อความจากตัวแทนบริษัท <span class="badge">10</span></a>
+        </li>
+        <li>
+            <a href="#"><i class="glyphicon glyphicon-user"></i> ข้อความจากนักศึกษา <span class="badge">10</span></a>
+        </li>
+        <li>
+            <a href="index.php?r=masmenu"><i class="glyphicon glyphicon-cog"></i> กลับหน้าจัดการข้อมูล </a>
+        </li>
+    </ul>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+            <a href="<?php echo Yii::app()->createUrl('site/logout') ?>">
+                <i class="fa fa-power-off"></i> ออกจากระบบ</a>
+        </li>
+    </ul>
+</nav>
