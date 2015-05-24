c4b45bb03f3231d53ff2fe9b3d9c8171e966089d
kimniyom <kimniyomclub@gmail.com>
2015-05-24 22:44:05 +0700

    new commit

--- protected/models/NewsModels.php
+++ protected/models/NewsModels.php
@@ -34,17 +34,24 @@ class NewsModels extends CActiveRecord {
     
     function Get_newsgeneralAll_admin() {
         $query = "SELECT n.*,c.collegian_name,c.collegian_lname
-            FROM News n INNER JOIN collegian c ON n.News_Owner = c.collegian_code
+            FROM News n LEFT JOIN collegian c ON n.News_Owner = c.collegian_code
             WHERE n.News_Catagory_id = '1' ORDER BY n.News_id DESC";
         return Yii::app()->db->createCommand($query)->queryAll();
     }
     
     function Get_newscollegianAll_admin() {
         $query = "SELECT n.*,c.collegian_name,c.collegian_lname
-            FROM News n INNER JOIN collegian c ON n.News_Owner = c.collegian_code
+            FROM News n LEFT JOIN collegian c ON n.News_Owner = c.collegian_code
             WHERE n.News_Catagory_id = '2' ORDER BY n.News_id DESC";
         return Yii::app()->db->createCommand($query)->queryAll();
     }
     
+    public function Get_News($type = "",$limit = ""){
+        $query = "SELECT n.*,c.collegian_name,c.collegian_lname
+        FROM News n INNER JOIN collegian c ON n.News_Owner = c.collegian_code
+        WHERE n.News_Catagory_id = '$type' ORDER BY n.News_id DESC LIMIT $limit";
+        
+        return Yii::app()->db->createCommand($query)->queryAll();
+    }
     
 }
--- protected/models/News_collegian.php
+++ protected/models/News_collegian.php
@@ -12,24 +12,34 @@ class News_collegian {
         return Yii::app()->db->createCommand($query)->queryRow();
     }
 
-    function Count_News_Genneral($collegian_code = '') {
+    function Count_News_Genneral() {
+        if (Yii::app()->session['user'] == "U") {
+            $where = " News_Owner = '" . Yii::app()->session['collegian_code'] . "' ";
+        } else {
+            $where = "1=1";
+        }
         $query = "SELECT COUNT(*) TOTAL "
                 . "FROM News "
-                . "WHERE News_Owner = '$collegian_code' AND News_Catagory_id = '1'";
+                . "WHERE $where AND News_Catagory_id = '1'";
         $rs = Yii::app()->db->createCommand($query)->queryRow();
-        
+
         return $rs['TOTAL'];
     }
-    
-    function Count_News_Collegian($collegian_code = '') {
+
+    function Count_News_Collegian() {
+        if (Yii::app()->session['user'] == "U") {
+            $where = " News_Owner = '" . Yii::app()->session['collegian_code'] . "' ";
+        } else {
+            $where = "1=1";
+        }
         $query = "SELECT COUNT(*) TOTAL "
                 . "FROM News "
-                . "WHERE News_Owner = '$collegian_code' AND News_Catagory_id = '2'";
+                . "WHERE $where AND News_Catagory_id = '2'";
         $rs = Yii::app()->db->createCommand($query)->queryRow();
-        
+
         return $rs['TOTAL'];
     }
-    
+
     function Get_newscollegianAll($collegian_code = '') {
         $query = "SELECT * FROM News WHERE News_Owner = '$collegian_code' AND News_Catagory_id = '2' ORDER BY News_id DESC";
         return Yii::app()->db->createCommand($query)->queryAll();
