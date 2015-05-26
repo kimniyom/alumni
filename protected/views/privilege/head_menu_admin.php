d98790db37e0dfea7f4b5781e3520bd668248b4f
kimniyom <kimniyomclub@gmail.com>
2015-05-26 17:04:02 +0700

    new commit

--- protected/views/privilege/head_menu_admin.php
+++ protected/views/privilege/head_menu_admin.php
@@ -3,12 +3,6 @@
         <li>
             <a href="index.php?r=site"><b><i class="fa fa-home"></i> หน้าเว็บ</b></a>
         </li>
-        <li style="padding: 0px;">
-            <a href="#stories"><i class="glyphicon glyphicon-comment"></i> ข้อความจากตัวแทนบริษัท <span class="badge">10</span></a>
-        </li>
-        <li>
-            <a href="#"><i class="glyphicon glyphicon-user"></i> ข้อความจากนักศึกษา <span class="badge">10</span></a>
-        </li>
         <li>
             <a href="index.php?r=masmenu"><i class="glyphicon glyphicon-cog"></i> กลับหน้าจัดการข้อมูล </a>
         </li>
