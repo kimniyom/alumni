f5a0d0e722a142ddc084eb0ce23eaf8f5e4c3bd2
mashimaro2010 <amnat_kalwtunkum@hotmail.com>
2015-05-27 16:06:07 +0700

    122121

--- protected/models/NewsModels.php
+++ protected/models/NewsModels.php
@@ -53,5 +53,38 @@ class NewsModels extends CActiveRecord {
 
         return Yii::app()->db->createCommand($query)->queryAll();
     }
+    
+     //ดึงมาให้หมดสำหรับ Admin ในหมวดข่าวทั่วไป  
+    function Get_newsgeneral_For_admin() {
+        $query = "SELECT n.*,IFNULL(c.collegian_name,adm.admin_name) as collegian_name,
+            IFNULL(c.collegian_lname,adm.admin_lname) as collegian_lname
+            FROM News n LEFT JOIN collegian c ON n.News_Owner = c.collegian_code
+            LEFT JOIN admin adm on adm.id=n.News_Owner
+            WHERE n.News_Catagory_id = '1' ORDER BY n.News_id DESC";
+        return Yii::app()->db->createCommand($query)->queryAll();
+    }
+
+    function Get_newscollegian_For_admin() {
+        $query = "SELECT n.*,IFNULL(c.collegian_name,adm.admin_name) as collegian_name,
+            IFNULL(c.collegian_lname,adm.admin_lname) as collegian_lname
+            FROM News n LEFT JOIN collegian c ON n.News_Owner = c.collegian_code
+            LEFT JOIN admin adm on adm.id=n.News_Owner
+            WHERE n.News_Catagory_id = '2' ORDER BY n.News_id DESC";
+        return Yii::app()->db->createCommand($query)->queryAll();
+    }
+
+    // Function ดึงมาแก้ไขข่าว
+    public function Get_News_Edit($News_id = '') {
+
+        $query = "SELECT n.*,CONCAT(c.collegian_name,' ',c.collegian_lname) as News_Owner,
+                Ncr.News_Catagory
+                FROM News n LEFT JOIN collegian c ON n.News_Owner = c.collegian_code
+                LEFT JOIN admin adm on adm.id=n.News_Owner
+                INNER JOIN News_Catagories Ncr ON Ncr.News_Catagory_id=n.News_Catagory_id
+                WHERE n.News_id = '$News_id' ";
+        $rs = Yii::app()->db->createCommand($query)->queryRow();
+        return $rs;
+    }
+
 
 }
