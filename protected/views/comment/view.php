57fc3b537a32cdcd864bcf487f31baa60b545c8a
kimniyom <kimniyomclub@gmail.com>
2015-05-28 17:01:25 +0700

    new commit

--- protected/views/comment/view.php
+++ protected/views/comment/view.php
@@ -12,11 +12,19 @@ foreach ($comment as $rs) {
     <div class="direct-chat-msg">
         <div class='direct-chat-info clearfix'>
             <span class='direct-chat-name pull-left'><?php echo $rs['name'] ?></span>
-            <span class='direct-chat-timestamp pull-right'><?php echo $lib->thaidate($rs['d_update']) ?></span>
+            <span class='direct-chat-timestamp pull-right'>
+                <?php echo $lib->thaidate($rs['d_update']) ?>
+            </span>
         </div><!-- /.direct-chat-info -->
         <img class="direct-chat-img" src="<?php echo Yii::app()->baseUrl; ?>/assets/jquery.picture.cut/uploads/<?php echo $img; ?>" alt="message user image" /><!-- /.direct-chat-img -->
         <div class="direct-chat-text" style=" color: #000;">
             <?php echo $rs['comment'] ?>
+            <span class='direct-chat-timestamp pull-right'>
+            <?php if (Yii::app()->session['collegian_code'] == $rs['collegian_code'] || Yii::app()->session['user'] == "A") { ?>
+                    <a href="#"><i class="fa fa-edit"></i> แก้ไข</a>
+                    <a href="#"><i class="fa fa-trash"></i> ลบ</a>
+                <?php } ?>
+            </span>
         </div><!-- /.direct-chat-text --> 
     </div><!-- /.direct-chat-msg -->
 <?php } ?>
