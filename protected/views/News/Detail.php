8392fb586a53b3829d916452a70f43cb2e95b5a6
kimniyom <kimniyomclub@gmail.com>
2015-05-28 15:08:50 +0700

    new commit

--- protected/views/News/Detail.php
+++ protected/views/News/Detail.php
@@ -1,3 +1,54 @@
+<style>
+    .album ul {         
+        padding:0 0 0 0;
+        margin:0 0 0 0;
+    }
+    .album ul li {     
+        list-style:none;
+        margin-bottom:25px;           
+    }
+    .album ul li img {
+        cursor: pointer;
+    }
+    .modal-body {
+        padding:5px !important;
+    }
+    .modal-content {
+        border-radius:0;
+    }
+    .modal-dialog img {
+        text-align:center;
+        margin:0 auto;
+    }
+    .controls{          
+        width:50px;
+        display:block;
+        font-size:11px;
+        padding-top:8px;
+        font-weight:bold;          
+    }
+    .next {
+        float:right;
+        text-align:right;
+    }
+    /*override modal for demo only*/
+    .modal-dialog {
+        max-width:760px;
+        padding-top: 90px;
+    }
+    @media screen and (min-width: 768px){
+        .modal-dialog {
+            width:760px;
+            padding-top: 90px;
+        }          
+    }
+    @media screen and (max-width:1500px){
+        #ads {
+            display:none;
+        }
+    }
+</style>
+
 <?php
 /* @var $this CollegianController */
 /* @var $model Collegian */
@@ -8,20 +59,50 @@ $this->breadcrumbs = array(
 );
 ?>
 
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">         
+            <div class="modal-body">                
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
 <div class="box box-warning">
     <div class="box-body">
-        <h3 style="margin-top: 0px;">
+        <h3 style="margin-top: 0px; color: #cc0000; font-weight: bold;">
             <?php echo $News['News_Head']; ?>
         </h3>
-        <div class="well well-sm">
-            <?php echo $News['News_Detail']; ?>
+        <div class="well well-sm" style=" background: none; position: relative;">
+            <center>
+                <?php
+                $news = new NewsModels();
+                $lib = new Lib();
+                $first = $news->Get_first_news($News['News_id']);
+                if ($first != 0) {
+                    ?>
+                    <img src="<?php echo Yii::app()->baseUrl; ?>/upload_news/<?php echo $first; ?>" class="img-responsive"/>
+                    <br/>
+                <?php } ?>
+            </center>
+            <?php echo $News['News_Detail']; ?><br/>
+            <p style=" float: right;">
+                อัพเดทเมื่อ 
+                <?php echo $lib->thaidate($News['CreateNews_Date']); ?>
+            </p><br/>
         </div>
 
         <!-- รูปภาพข่าว -->
-        <div class="row">
-            <center>
-                <img src="<?php echo Yii::app()->baseUrl; ?>/images/slider-bg.png" width="100" class="responsive"/>
-            </center>
+        <div class="container">
+            <div class="album">
+                <ul>
+                    <?php foreach ($news_images as $img_news): ?>
+                        <li class="col-lg-2 col-md-2 col-sm-3 col-xs-4">
+                            <img src="<?php echo Yii::app()->baseUrl; ?>/upload_news/<?php echo $img_news['News_Image'] ?>" class="img-responsive"/>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
         </div>
 
         <hr>
@@ -76,13 +157,32 @@ $this->breadcrumbs = array(
 
 
     </div>
-    <div class="box-footer">
-        <label>ข่าวที่เกี่ยวข้อง</label>
+
+</div>
+
+<div class="box box-default">
+    <div class="box-header with-border">
+        ข่าวที่เกี่ยวข้อง
+    </div>
+    <div class="container-fluid" style=" margin-top: 10px;">
         <?php foreach ($News_jam as $newsAll) { ?>
-            <p>
+            <div class="col-md-3 col-sm-3">
+                <?php
+                $first_jam = $news->Get_first_news($newsAll['News_id']);
+                if ($first_jam != 0) {
+                    ?>
+                    <img src="<?php echo Yii::app()->baseUrl; ?>/upload_news/<?php echo $first_jam ?>" class="img-responsive"/>
+                <?php } else { ?>
+                    <img src="<?php echo Yii::app()->baseUrl; ?>/images/no_photo_icon.png" class="img-responsive"/>
+                <?php } ?>
                 <a href="index.php?r=News/Detail_News&News_id=<?php echo $newsAll['News_id'] ?>">
-                    <?php echo $newsAll['News_Head']; ?></a>
-            </p>
+                    <?php
+                    echo $newsAll['News_Head'];
+                    ?>
+                </a>
+            </div>
+
         <?php } ?>
     </div>
 </div>
+
