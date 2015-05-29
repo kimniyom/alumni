aac10f65fa06eb6efea9f40b77a6728ba2a22cc3
kimniyom <kimniyomclub@gmail.com>
2015-05-29 17:18:15 +0700

    new commit

--- protected/views/comment/view.php
+++ protected/views/comment/view.php
@@ -1,3 +1,61 @@
+<script type="text/javascript">
+    function popup_edit_comment(id, comment) {
+        $("#comment_id").val(id);
+        $("#comment").val(comment);
+        $("#edit_comment").modal('show');
+    }
+
+    function edit_comment() {
+        var url = "index.php?r=comment/edit_comment";
+        var id = $("#comment_id").val();
+        var comment = $("#comment").val();
+        var data = {id: id, comment: comment};
+        if (comment == '') {
+            $("#comment").focus();
+            return false;
+        }
+
+        $.post(url, data, function (success) {
+            window.location.reload();
+        });
+    }
+
+    function del_comment(id) {
+        var r = confirm("คุณต้องการลบความคิดเห็น ใช่ หรือ ไม่ ...? ");
+        if (r == true) {
+            var url = "index.php?r=comment/delete_comment";
+            var data = {id: id};
+            $.post(url, data, function (success) {
+                window.location.reload();
+            });
+        }
+    }
+
+</script>
+
+<div class="modal fade" id="edit_comment">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">แก้ไขความคิดเห็น</h4>
+            </div>
+            <div class="modal-body">
+                <label>ความคิดเห็นที่ </label>
+                <input type="text" id="comment_id" class="form-control input-sm" readonly="readonly"/>
+                <label>
+                    ความคิดเห็น
+                </label>
+                <textarea id="comment" name="comment" class="form-control"></textarea>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default btn-sm" data-dismiss="modal"><i class="fa fa-remove"></i>ปิด</button>
+                <button type="button" class="btn btn-primary btn-sm" onclick="edit_comment();"><i class="fa fa-save"></i> แก้ไข</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
 <!-- Message. Default to the left -->
 <?php
 $collegian = new Collegian();
@@ -20,9 +78,9 @@ foreach ($comment as $rs) {
         <div class="direct-chat-text" style=" color: #000;">
             <?php echo $rs['comment'] ?>
             <span class='direct-chat-timestamp pull-right'>
-            <?php if (Yii::app()->session['collegian_code'] == $rs['collegian_code'] || Yii::app()->session['user'] == "A") { ?>
-                    <a href="#"><i class="fa fa-edit"></i> แก้ไข</a>
-                    <a href="#"><i class="fa fa-trash"></i> ลบ</a>
+                <?php if (Yii::app()->session['collegian_code'] == $rs['collegian_code'] || Yii::app()->session['user'] == "A") { ?>
+                    <a href="javascript:popup_edit_comment('<?php echo $rs['id'] ?>','<?php echo $rs['comment'] ?>');"><i class="fa fa-edit"></i> แก้ไข</a>
+                    <a href="javascript:del_comment('<?php echo $rs['id'] ?>');"><i class="fa fa-trash"></i> ลบ</a>
                 <?php } ?>
             </span>
         </div><!-- /.direct-chat-text --> 
